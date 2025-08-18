7a236afbc88c2e5c356c0691017f153315f6e7b8
Matthew van der Hoorn <md.registrations@protonmail.com>
2025-08-18 15:40:03 +0200

    Clean slate

--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "resonance/resonance.h"
-
-int main(void) {
-    printf("[resonance] Hello from demo app!\n");
-    printf("[resonance] add(2, 3) = %d\n", resonance_add(2, 3));
-    printf("[resonance] version = %s\n", resonance_version_string());
-    return 0;
-}
